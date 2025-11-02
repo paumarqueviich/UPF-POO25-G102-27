62c0dffc8300021d0afbf1e717194757e6f09aea
Pau Marquevich <pau.marquevich01@estudiant.upf.edu>
2025-11-02 21:21:56 +0100

    Subo el Lab2 completo

---
+++ Lab2/NormalizedDataset.java
@@ -0,0 +1,27 @@
+public class NormalizedDataset extends Dataset {
+    private final Vector minInput;
+    private final Vector maxInput;
+    private final double minOutput;
+    private final double maxOutput;
+
+    public NormalizedDataset(Vector minInput, Vector maxInput, double minOutput, double maxOutput) {
+        this.minInput = minInput;
+        this.maxInput = maxInput;
+        this.minOutput = minOutput;
+        this.maxOutput = maxOutput;
+    }
+
+    public Record transform(Record r) {
+        int dim = minInput.getDim();
+        double[] newInput = new double[dim];
+        for (int i = 0; i < dim; i++) {
+            double range = maxInput.getValues()[i] - minInput.getValues()[i];
+            if (range == 0) range = 1;
+            newInput[i] = (r.getInput().getValues()[i] - minInput.getValues()[i]) / range;
+        }
+        double rangeY = maxOutput - minOutput;
+        if (rangeY == 0) rangeY = 1;
+        double newOutput = (r.getOutput() - minOutput) / rangeY;
+        return new Record(new Vector(dim, newInput), newOutput);
+    }
+}
